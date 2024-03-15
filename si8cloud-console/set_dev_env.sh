36201175b567364f813a4f3bb7186c6ba204a0e2
Flavio Siotto <flavio.siotto@gmail.com>
2024-03-15 20:54:40 +0100

    move to workspace si8cloud

---
+++ si8cloud-console/set_dev_env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+export POSTGRES_DATA_DIR=/home/flavio/playground/si8cloud-console-be/data/db/
+export POSTGRES_DB=postgres
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+
+export PGADMIN_DIR=/home/flavio/playground/si8cloud-console-be/pgadmin-data/
+export PGADMIN_EMAIL=admin@local.com
+export PGADMIN_PASSWORD=secret
+
+
+export SI8CLOUD_FE_SRC_DIR=/home/flavio/playground/si8cloud-console-fe/
