0c71e00da49b5cd0ccd53bf5eeb859d644197270
Flavio Siotto <flavio.siotto@gmail.com>
2024-03-17 11:01:15 +0100

    devenv settings

--- si8cloud-console/set_dev_env.sh
+++ si8cloud-console/set_dev_env.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-
-export POSTGRES_DATA_DIR=/home/flavio/playground/si8cloud-console-be/data/db/
+export POSTGRES_DATA_DIR=/home/flavio/playground/si8cloud-data/postgresql/data/
 export POSTGRES_DB=postgres
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 
-export PGADMIN_DIR=/home/flavio/playground/si8cloud-console-be/pgadmin-data/
+export PGADMIN_DIR=/home/flavio/playground/si8cloud-data/pgadmin/
 export PGADMIN_EMAIL=admin@local.com
 export PGADMIN_PASSWORD=secret
 
 
-export SI8CLOUD_FE_SRC_DIR=/home/flavio/playground/si8cloud-console-fe/
+export SI8CLOUD_FE_SRC_DIR=$(pwd)/si8cloud-console-fe
+export SI8CLOUD_BE_SRC_DIR=$(pwd)/si8cloud-console-be
